5129dc77f9939b4b409a75a1a1c2c51b2aac0dfd
hyun0430 <138427291+hyun0430@users.noreply.github.com>
2023-08-09 14:28:31 +0900

    수정
    수정함

---
+++ frontend/login.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#login-form");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+
+  const res = await fetch("/login", {
+    method: "post",
+    body: formData,
+  });
+  const data = await res.json();
+};
+
+form.addEventListener("submit", handleSubmit);
